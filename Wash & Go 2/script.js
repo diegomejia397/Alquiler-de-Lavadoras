f549f81212758347e0cea20b4b6c8d7e785272e8
diegomejia397 <mejiavillegas2009@gmail.com>
2025-03-06 16:59:04 -0500

    Add files via upload

---
+++ Wash & Go 2/script.js
@@ -0,0 +1,4 @@
+function alquilarProducto(producto) {
+    alert("Has elegido alquilar: " + producto + ". ¡Nos pondremos en contacto contigo!");
+    window.location.href = "https://wa.me/3177335546?text=Quiero alquilar la " + producto;
+}
